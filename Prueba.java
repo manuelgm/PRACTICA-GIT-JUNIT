9e67eae6dac7f1bd1aeff81f9b2f32f0eb9c529a
manuelgm <manuelgm7@uma.es>
2015-06-01 18:56:59 +0200

    Version2. Modificado Prueba.java, con el ejercicio 1

--- Prueba.java
+++ Prueba.java
@@ -4,9 +4,28 @@ import org.junit.*;
 
 public class Prueba {
 
+	private BinaryString bitset1, bitset2, bitset3, bitset4, bitset5;
+	private String pruebaString2 = "1010101010", pruebaString3 = "100101",
+			pruebaString4 = "0110101", pruebaString5 = "1111000";
+
+	// Para cada Test inicializamos antes su correspondiente bitset con su
+	// String asociado.
+
+	@Before
+	public void antesDelTest() {
+		bitset1 = new BinaryString(10);
+		bitset2 = new BinaryString(pruebaString2);
+		bitset3 = new BinaryString(pruebaString3);
+		bitset4 = new BinaryString(pruebaString4);
+		bitset5 = new BinaryString(pruebaString5);
+	}
+
+	// Comprobamos si al inicializar el bitset (con los dos constructores) el
+	// tama–o es el esperado.
 	@Test
-	public void test() {
-		fail("Noy yet implemented");
+	public void testLength() {
+		assertEquals(10, bitset1.length());
+		assertEquals(10, bitset2.length());
 	}
 
 }
